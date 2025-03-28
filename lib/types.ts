8e8080451d017b5b7ebf377efbfe40fd83917b8d
gameup <cedric.balzer02@gmail.com>
2025-03-28 20:39:31 +0100

    Mocks and help functions

--- lib/types.ts
+++ lib/types.ts
@@ -9,3 +9,32 @@ export interface EventInfo {
     attendees: number,
     image: string,
 }
+export interface Organization {
+    id: string;
+    numOfMembers: number;
+    numOfEvents: number;
+    owner: string;
+    createdAt: string;
+    updatedAt: string;
+    updatedBy: string;
+    name: string;
+    address: string;
+    description: string;
+    profilePicture: string;
+    website: string;
+}
+
+export interface User {
+    id: string;
+    name: string;
+    email: string;
+    createdAt: string;
+    updatedAt: string;
+    profilePicture: string;
+}
+
+export interface OrgUser {
+    user: User;
+    role: string;
+    orgId: string;
+}
